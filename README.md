44e7b55655c8e61acf330b9c0927dd773d3ebc2c
Anderson Carneiro Sousa <acarneirosousa@gmail.com>
2022-10-17 10:57:35 -0300

    Creating Tables

---
+++ README.md
@@ -0,0 +1,44 @@
+- [x] Modelar uma aplicação contendo entre 8 e 10 tabelas, disponibilize a imagem da modelagem e a estrutura SQL de criação da tabelas
+
+  ![Modelagem do Banco](./assets/Diagrama.PNG)
+
+- [ ] Todas as tabelas devem possuir pelo menos dez registros;
+
+- [ ] Implementar ao menos cinco views com inner join;
+	- [ ] Necessário pelo menos dois inner joins contemplando duas tabelas;
+	- [ ] Necessário pelo menos mais dois inner joins contemplando três
+tabelas;
+	- [ ] Necessário pelo menos um inner join contemplando cinco tabelas ou
+mais.
+
+- [ ] Criar uma view com Left Join;
+
+- [ ] Criar uma view com Right Join;
+
+- [ ] Implementar o uso de pelo menos duas triggers;
+
+- [ ] Implementar o uso de pelo menos dez procedures:
+	- [ ] Pelo menos três para realizar cadastros;
+	- [ ] Pelo menos três para alterar;
+	- [ ] Pelo menos três para remover.
+
+- [ ] Todas as tabelas deverão disponibilizar comandos completos para: cadastrar,
+alterar e remover dados;
+
+- [ ] Criar comandos DQL contendo as seguintes expressões:
+	- [ ] WHERE;
+	- [ ] AND;
+	- [ ] OR;
+	- [ ] BETWEEN;
+	- [ ] ORDER BY;
+	- [ ] GROUP BY;
+	- [ ] HAVING;
+	- [ ] LIKE;
+	- [ ] COUNT;
+	- [ ] MAX;
+	- [ ] MIN;
+	- [ ] IF;
+	- [ ] CASE;
+	- [ ] NOW;
+	- [ ] DATE_FORMAT;
+	- [ ] Subqueries (utilização de dois comandos SQL na mesma query)
---
+++ create_tables.sql
@@ -0,0 +1,72 @@
+create table clientes(
+	codigo int primary key auto_increment,
+	nome varchar(30),
+	email varchar(30) unique
+);
+
+create table funcionarios(
+	codigo int primary key auto_increment,
+	nome varchar(30),
+	cpf varchar(11) unique
+);
+
+create table filiais(
+	codigo int primary key auto_increment,
+	nome varchar(30),
+	cnpj varchar(14) 
+);
+
+-- create table pdvs (
+-- 	codigo int primary key,
+-- 	filialId int,
+-- 	FOREIGN KEY (filialId) REFERENCES filiais(codigo)
+-- );
+
+create table categorias(
+	codigo int primary key auto_increment,
+	nome varchar(30) 
+);
+
+create table marcas(
+	codigo int primary key auto_increment,
+	nome varchar(30) 
+);
+
+create table produtos(
+	codigo int primary key auto_increment,
+	nome varchar(30),
+	codigoMarca int,
+	FOREIGN KEY (codigoMarca) REFERENCES marcas(codigo),
+	codigoCategoria int,
+	FOREIGN KEY (codigoCategoria) REFERENCES categorias(codigo),
+	codigoFilial int,
+	FOREIGN KEY (codigoFilial) REFERENCES filiais(codigo),
+	quantidade float,
+	precoMedioCompra float
+);
+
+create table vendas(
+	codigo int UNIQUE,
+	dataHora datetime,
+	codigoFilial int,
+	FOREIGN KEY (codigoFilial) REFERENCES filiais(codigo),
+	codigoFuncionario int,
+	FOREIGN KEY (codigoFuncionario) REFERENCES funcionarios(codigo),
+	codigoCliente int,
+	FOREIGN KEY (codigoCliente) REFERENCES clientes(codigo),
+	totalVenda float
+);
+
+create table movimentacoesProdutos(
+	codigo int primary key auto_increment,
+	tipo varchar(10),
+	codigoVenda int,
+	FOREIGN KEY (codigoVenda) REFERENCES vendas(codigo),
+	codigoFilial int,
+	FOREIGN KEY (codigoFilial) REFERENCES filiais(codigo),
+	codigoProduto int,
+	FOREIGN KEY (codigoProduto) REFERENCES produtos(codigo),
+	dataHora datetime,
+	quantidade float,
+	preco float
+);
