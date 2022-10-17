2e9bd8f908e52cee370fcfabba1b9b872947af6b
Anderson Carneiro Sousa <acarneirosousa@gmail.com>
2022-10-17 16:56:30 -0300

    adding update tables

--- sql/3_populate_tables.sql
+++ sql/3_populate_tables.sql
@@ -11,6 +11,10 @@ VALUES
 ("Cliente9", "cliente9@gmail.com"),
 ("Cliente10", "cliente10@gmail.com");
 
+update clientes
+set nome = "Cliente01"
+where nome = "Cliente1"
+
 INSERT INTO vendedores (nome)
 VALUES
 ("Vendedor1"),
@@ -23,6 +27,10 @@ VALUES
 ("Vendedor8"),
 ("Vendedor9"),
 ("Vendedor10");
+
+update vendedores
+set nome = "Vendedor01"
+where nome = "Vendedor1"
 
 INSERT INTO filiais (nome, cnpj)
 VALUES
@@ -37,6 +45,10 @@ VALUES
 ("Filial9", "11111111111119"),
 ("Filial10", "11111111111110");
 
+update filiais
+set nome = "Filiais01"
+where nome = "Filiais1"
+
 INSERT INTO categorias (nome)
 VALUES
 ("Categoria1"),
@@ -49,6 +61,10 @@ VALUES
 ("Categoria8"),
 ("Categoria9"),
 ("Categoria10");
+
+update categorias
+set nome = "categoria01"
+where nome = "categoria1"
 
 INSERT INTO marcas (nome)
 VALUES
@@ -63,6 +79,10 @@ VALUES
 ("Marca9"),
 ("Marca10");
 
+update marcas
+set nome = "marca01"
+where nome = "marca1"
+
 --Produtos
 call pCriarProduto("P1M1", 1, 1, 1);
 call pCriarProduto("P2M2", 2, 2, 2);
@@ -74,6 +94,10 @@ call pCriarProduto("P7M7", 7, 7, 7);
 call pCriarProduto("P8M8", 8, 8, 8);
 call pCriarProduto("P9M9", 9, 9, 9);
 call pCriarProduto("P10M10", 10, 10, 10);
+
+update produtos
+set nome = "p01"
+where nome = "p1m1"
 
 --vendas
 call pCriarVenda(1, 1, 1);
@@ -87,6 +111,10 @@ call pCriarVenda(8, 8, 8);
 call pCriarVenda(9, 9, 9);
 call pCriarVenda(10, 10, 10);
 
+update vendas
+set filial = 2
+where codigo = 1
+
 --MovimentalçaoProduto
 call pCriarMovimentacaoEntrada(1, 1, 10, 1);
 call pCriarMovimentacaoEntrada(2, 2, 20, 2);
@@ -98,3 +126,7 @@ call pCriarMovimentacaoSaida(2, 2, 2, 1, 2.10);
 call pCriarMovimentacaoSaida(3, 3, 3, 1, 3.10);
 call pCriarMovimentacaoSaida(4, 4, 4, 1, 4.10);
 call pCriarMovimentacaoSaida(5, 5, 5, 1, 5.10);
+
+update movimentacoesProdutos
+set codigoVenda = 2 
+where codigo = 1
